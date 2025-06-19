80d5141d2fb89385b65bb44f4be201356d9c45ec
Wilmer Sanchez Allende <wilmer.sanchez@unprg.edu.pe>
2025-06-19 16:06:28 -0500

    Configuracion docker usuarios

--- msvc-usuarios/src/main/java/com/wilmer/springcloud/msvc/usuarios/msvc_usuarios/clients/CursoClienteRest.java
+++ msvc-usuarios/src/main/java/com/wilmer/springcloud/msvc/usuarios/msvc_usuarios/clients/CursoClienteRest.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-cursos", url = "localhost:8002")
+// @FeignClient(name = "msvc-cursos", url = "localhost:8002") // Este es por ejecuta local
+@FeignClient(name = "msvc-cursos", url = "host.docker.internal:8002") // este invoca al mscv interno de cursp , es decir curso no esta dockerizado
 public interface CursoClienteRest {
     @DeleteMapping("/eliminar-curso-usuario/{id}")
     void eliminarCursoUsuarioPorId(@PathVariable Long id);
